fea17ef8e1b44d2b0c8aa382544b9c4d7302888f
Josue Vitor Porto Lobo <josuevitorp.lobo@gmail.com>
2025-10-04 15:00:10 -0300

    feat: add handler post

--- submissions/RoyMusthang/internal/server/routes.go
+++ submissions/RoyMusthang/internal/server/routes.go
@@ -1,14 +1,34 @@
 package server
 
 import (
+	"encoding/json"
 	"net/http"
+
+	"RoyMusthang/internal/entity"
+	"RoyMusthang/internal/repository"
+	"RoyMusthang/internal/service"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
+type Handler struct {
+	service *service.PaymentService
+}
+
+func NewHandler(service *service.PaymentService) *Handler {
+	return &Handler{service: service}
+}
+
 func (s *Server) RegisterRoutes() http.Handler {
 	r := gin.Default()
+
+	failureRate := 0.1 // 10% de falha
+
+	repo := repository.NewPaymentRepository()
+	pixService := service.NewPIXService(failureRate)
+	paymentService := service.NewPaymentService(repo, pixService)
+	handler := NewHandler(paymentService)
 
 	r.Use(cors.New(cors.Config{
 		AllowOrigins:     []string{"*"},
@@ -18,7 +38,7 @@ func (s *Server) RegisterRoutes() http.Handler {
 	}))
 
 	r.GET("/", s.HelloWorldHandler)
-	r.POST("/payouts/batch", s.ProcessBatch)
+	r.POST("/payouts/batch", handler.ProcessBatch)
 
 	r.GET("/health", s.healthHandler)
 
@@ -32,8 +52,28 @@ func (s *Server) HelloWorldHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, resp)
 }
 
-func (s *Server) ProcessBatch(c *gin.Context) {
-	c.JSON(http.StatusOK, s.db.Health())
+func (s *Handler) ProcessBatch(c *gin.Context) {
+	var req entity.BatchRequest
+	if err := json.NewDecoder(c.Request.Body).Decode(&req); err != nil {
+		c.JSON(http.StatusBadRequest, req)
+		return
+	}
+
+	// Validate request
+	if req.BatchID == "" {
+		c.JSON(http.StatusBadRequest, req)
+		return
+	}
+	if len(req.Items) == 0 {
+		c.JSON(http.StatusBadRequest, req)
+		return
+	}
+
+	// Process batch
+	response := s.service.ProcessBatch(req)
+
+	// Return response
+	c.JSON(http.StatusOK, response)
 }
 
 func (s *Server) healthHandler(c *gin.Context) {
