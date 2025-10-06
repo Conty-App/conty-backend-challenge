33509a92d53ef3cebbc93b8cc95a63d4e8136082
Pedro Alves <pedromoraes.junior96@gmail.com>
2025-10-06 00:47:26 -0300

    Update database.py
    - Finalizada configuração no banco PostgreSQL

--- submissions/pedro-alvesjr/db/database.py
+++ submissions/pedro-alvesjr/db/database.py
@@ -2,7 +2,7 @@ from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 from db.models import Base
 
-DATABASE_URL = "postgresql+asyncpg://usuario:senha@localhost:5432/seu_banco"
+DATABASE_URL = "postgresql+asyncpg://postgres:his1032@localhost:5432/conty_db"
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 
