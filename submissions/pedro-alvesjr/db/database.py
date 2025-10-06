7e8d6ca215fa1448243762cfb1a8b85b882af8d6
Pedro Alves <pedromoraes.junior96@gmail.com>
2025-10-05 23:19:37 -0300

    Criar database.py

---
+++ submissions/pedro-alvesjr/db/database.py
@@ -0,0 +1,8 @@
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker
+import os
+
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+asyncpg://conty:conty@localhost/conty_db")
+
+engine = create_async_engine(DATABASE_URL, echo=True)
+AsyncSessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
