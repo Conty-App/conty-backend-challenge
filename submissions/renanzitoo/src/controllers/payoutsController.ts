ec3133400f76bfe0d7b58fe2e79056d0836a0988
Renan Costa <91814882+renanzitoo@users.noreply.github.com>
2025-10-06 08:48:26 -0300

    feat: add getAllPayouts and getPayoutById

--- submissions/renanzitoo/src/controllers/payoutsController.ts
+++ submissions/renanzitoo/src/controllers/payoutsController.ts
@@ -1,6 +1,7 @@
-import {Request, Response} from 'express';
+import e, {Request, Response} from 'express';
 import { handleBatch } from '../services/payoutsService';
 import { PayoutBatch } from '../types/payout';
+import { getAllPayments, getPaymentById } from '../models/paymentStore';
 
 export async function processBatch(req: Request, res: Response): Promise<void> {
   try {
@@ -8,6 +9,31 @@ export async function processBatch(req: Request, res: Response): Promise<void> {
     const result = await handleBatch(batchData);
     res.json(result);
   }catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+}
+
+export async function getAllPayouts(req: Request, res: Response): Promise<void> {
+  try {
+    const payouts = await getAllPayments();
+    res.json({total: payouts.length, payouts});
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+}
+
+export async function getPayoutById(req: Request, res: Response): Promise<void> {
+  const { external_id } = req.params;
+  try{
+    const payment = await getPaymentById(external_id);
+    if(!payment){
+      res.status(404).json({error: 'Payment not found'});
+    }else{
+      res.json(payment);
+    }
+  } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Internal Server Error' });
   }
--- submissions/renanzitoo/src/routes/payouts.ts
+++ submissions/renanzitoo/src/routes/payouts.ts
@@ -1,8 +1,10 @@
 import {Router} from 'express'
-import {processBatch} from '../controllers/payoutsController'
+import {getAllPayouts, getPayoutById, processBatch} from '../controllers/payoutsController'
 
 const router = Router();
 
 router.post('/batch', processBatch);
+router.get('/:external_id', getPayoutById);
+router.get('/', getAllPayouts);
 
 export default router;
