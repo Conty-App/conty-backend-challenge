c219891dd866a848eecdc6592b68ac5d231268d5
João Pedro Cordeiro <jpc5626@gmail.com>
2025-10-06 00:52:41 -0300

    feat: update controller

--- submissions/joaopedrorc/reco/src/controllers/recommendations.controller.ts
+++ submissions/joaopedrorc/reco/src/controllers/recommendations.controller.ts
@@ -1,19 +1,20 @@
 import { Request, Response, NextFunction } from 'express';
 import * as recommendationService from '../services/recommendation.service';
+import { RecommendationRequestType } from '../types';
+import { mapCampaignPayloadToCampaign } from '../utils/mappers.utils';
 
-export async function getRecommendations(
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) {
+export async function getRecommendations(req: Request, res: Response, next: NextFunction) {
   try {
-    const { campaign, top_k } = req.body;
-    if (!campaign) {
+    const requestPayload: RecommendationRequestType = req.body;
+
+    if (!requestPayload.campaign) {
       return res.status(400).json({ message: 'Campaign object is required' });
     }
 
-    const result = await recommendationService.findTopCreators(campaign, top_k);
+    const campaign = mapCampaignPayloadToCampaign(requestPayload.campaign);
+    const top_k = requestPayload.top_k;
 
+    const result = await recommendationService.findTopCreators(campaign, top_k);
     res.status(200).json(result);
   } catch (error) {
     next(error);
